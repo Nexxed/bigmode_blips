3000438c4efe583989d60aaaa22e1ad7fc3bfe5a
Nexxed <nexxed@memeware.net>
2020-01-30 21:05:40 +0000

    Added resource files

---
+++ client.lua
@@ -0,0 +1,156 @@
+-- if you've opted to disable player names in server.lua and
+-- then the script will use this name instead
+-- %i is replaced with the players' Server ID
+local defaultDormantNameFormat = "Dormant Player #%i"
+
+
+--- [[ CODENS HAPPENING HERE! DONT TOUCH! ]] ---
+local playerBlipHandles = {}
+local latestBlipsUpdate = {}
+local evaluationPlayers = {}
+
+exports("GetDormantBlipHandles", function()
+    return playerBlipHandles
+end)
+
+exports("GetLatestBlipsUpdate", function()
+    return latestBlipsUpdate
+end)
+
+local function has_value(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+function table.filter(table, it)
+    local ret = {}
+    for key, value in pairs(table) do
+        if it(value, key, table) then
+            ret[key] = value
+        end
+    end
+
+    return ret
+end
+
+local function GetServerPlayerIds()
+    -- get all the players that are currently in the same instance
+    -- as the local player ("active" players)
+    local rawPlayers = GetActivePlayers()
+
+    -- make a new table for storing player server IDs
+    local playerIDs = {}
+
+    -- iterate raw_players using ipairs and insert their server ID
+    -- into the player_ids table
+    for index, player in ipairs(rawPlayers) do
+        table.insert(playerIDs, GetPlayerServerId(player))
+    end
+
+    return playerIDs
+end
+
+local function Evaluate()
+
+    -- iterate through the players needed to be removed
+    for _, player in ipairs(evaluationPlayers) do
+
+        -- iterate through every blip handle we currently have stored
+        for player, blip in pairs(playerBlipHandles) do
+
+            -- filter through the most recent blips update for blips belonging to this player
+            local results = table.filter(latestBlipsUpdate, function(blip, key, blips)
+                return blip[1] == player
+            end)
+
+            -- if there are no blips belonging to this player, attempt to delete and dispose of handle
+            if(#results == 0 and playerBlipHandles[player] ~= nil) then
+
+                -- check if the blip handle does actually exist, if so, delete it
+                if(DoesBlipExist(playerBlipHandles[player])) then
+                    RemoveBlip(playerBlipHandles[player])
+                end
+
+                -- remove the player blip handle from storage
+                playerBlipHandles[player] = nil
+            end
+        end
+    end
+
+    evaluationPlayers = {}
+end
+
+-- this is called when a player leaves the server so clients can clean-up old blips
+RegisterNetEvent("_bigmode:evaluateBlips")
+AddEventHandler("_bigmode:evaluateBlips", function(player)
+    table.insert(evaluationPlayers, player)
+end)
+
+-- this is called when the server sends a new or updated batch of player data
+RegisterNetEvent("_bigmode:updateBlips")
+AddEventHandler("_bigmode:updateBlips", function(blips)
+    latestBlipsUpdate = blips
+
+    if(#evaluationPlayers >= 1) then
+        Evaluate()
+    end
+
+    Citizen.CreateThread(function()
+        (function()
+            -- iterate all dormant blips sent to us by the server
+            for index, blip in pairs(blips) do
+                local player = blip[1]
+                local playerPed = NetworkDoesEntityExistWithNetworkId(blip[2]) and NetworkGetEntityFromNetworkId(blip[2]) or nil
+
+                -- if the player isn't in our instance and isn't our local player, draw this blip
+                if((not DoesEntityExist(playerPed)) and player ~= GetPlayerServerId(PlayerId())) then
+
+                    -- get the players name and coords from the blip
+                    local playerName = blip[3]
+                    local coords = blip[4]
+
+                    -- sanity check in-case the player name isn't sent by server
+                    if(coords == nil) then
+                        coords = playerName
+                        playerName = nil
+                    end
+
+                    -- check if a blip already exists on the map for this player and
+                    -- if there is then use the stored handle, otherwise, create a
+                    -- new one and add it to the playerBlipHandles table
+                    local blip = 0
+                    if(playerBlipHandles[player] ~= nil and DoesBlipExist(playerBlipHandles[player])) then
+                        blip = playerBlipHandles[player]
+
+                        -- update the blips position
+                        SetBlipCoords(blip, coords[1], coords[2], coords[3])
+                    else
+                        -- create the new blip
+                        blip = AddBlipForCoord(coords[1], coords[2], coords[3])
+
+                        -- set the dormant players' blip properties
+                        SetBlipAlpha(blip, 180)
+                        SetBlipSprite(blip, 1)
+                        SetBlipScale(blip, 0.8)
+                        SetBlipShrink(blip, 1)
+                        SetBlipCategory(blip, 7)
+                        SetBlipDisplay(blip, 6)
+
+                        -- store the blip handle in the playerBlipHandles table using the players' server ID as key
+                        playerBlipHandles[player] = blip
+                    end
+
+                    -- set the blip name to the players' name, or the default dormant name if not sent by the server
+                    BeginTextCommandSetBlipName("STRING")
+                    AddTextComponentString(playerName ~= nil and playerName or string.format(defaultDormantNameFormat, player))
+                    EndTextCommandSetBlipName(blip)
+                end
+            end
+        end)()
+    end)
+end)
---
+++ server.lua
@@ -0,0 +1,171 @@
+-- how many milliseconds to wait before sending updates to all players again
+-- based on player count for key, so you can slow down updates depending on how many players are connected
+-- for 5 updates per second use 1000 / 5
+local updateIntervals = {
+    [256]   = 1000 * 5,     -- once every 5 seconds | during 256-1024 players
+    [128]   = 1000 * 4,     -- once every 4 seconds | during 128-256 players
+    [96]    = 1000 * 3,     -- once every 3 seconds | during 96-128 players
+    [64]    = 1000 * 2,     -- once every 2 seconds | during 64-96 players
+    [0]     = 1000          -- once every second    | during 0-64 players
+}
+
+-- how many milliseconds to wait before sending an update to the next player in-loop
+-- example: player A gets sent an update, server waits X milliseconds before sending to player B
+-- can be useful for bandwidth reasons
+local updateSpacing = 100
+
+-- whether or not to transmit player names with blip updates
+-- setting this to false will disable player names on blips!
+-- works the same way as updateIntervals
+local sendNames = {
+    [0]     = true          -- when the player count is between 0 and 96, names will be enabled
+}
+-- this is currently unfinished, so just set 0 to whatever value you want for now
+
+
+
+-- debug threshold limits
+-- if enabled, it will notify you via server console that the server isn't meeting performance expectations
+-- this is usually due to improper interval configuration that doesn't match your servers hardware
+
+-- its recommended to leave this disabled unless you're testing high player counts or similar
+-- since it can be used to identify if the script can "keep up" with the player count
+local threadTimeWarnings = true
+local mainThreadTimeThreshold = 10          -- parent thread
+local updateThreadTimeThreshold = 10        -- blip updates thread
+
+
+
+--- [[ CODENS HAPPENING HERE! DONT TOUCH! ]] ---
+local lastBlipsUpdate = {}
+local lastIntervalValue = 0
+local shouldSendNames = false -- no, changing this instead of the option above won't do anything
+
+function math.clamp(low, n, high)
+    return math.min(math.max(n, low), high)
+end
+
+-- emitted when a player leaves the server
+AddEventHandler("playerDropped", function()
+    TriggerClientEvent("_bigmode:evaluateBlips", -1, source)
+end)
+
+-- this is the main update thread for pushing blip location updates to players
+Citizen.CreateThread(function()
+    while true do
+        local mt_begin = GetGameTimer()
+
+        -- get and store all of the currently-connected players
+        local players = GetPlayers()
+
+
+        -- iterate through the configured intervals and find the one that
+        -- best suits the current number of players
+        local updateInterval = 0
+        local updateIntervalLimit = 0
+        for limit, interval in pairs(updateIntervals) do
+            if(limit <= #players) then
+                updateInterval = interval
+                updateIntervalLimit = limit
+            end
+        end
+
+        if(lastIntervalValue ~= updateIntervalLimit) then
+            lastIntervalValue = updateIntervalLimit
+            print(string.format("[^2BigMode^7] Updated blip update interval to ^2%dms (%d) ^7due to ^2%d ^7players being connected.", updateInterval, updateIntervalLimit, #players))
+        end
+
+
+        -- iterate through the configured sendNames limits and find the one that
+        -- best suits the current number of players
+        local sendNamesLimit = 0
+        for limit, sendName in pairs(sendNames) do
+            if(limit <= #players) then
+                shouldSendNames = sendName
+                sendNamesLimit = limit
+            end
+        end
+
+
+        if(#players > 0) then
+
+            -- this is where the heavy-lifting is done in the loop
+            -- so we create a new thread so we can quickly move-on without waiting
+            Citizen.CreateThread(function()
+                local up_begin = GetGameTimer()
+
+                players = GetPlayers()
+
+                -- iterate through the players table above and build an event object
+                -- that includes the players' server ID and their in-game position
+                local blips = {}
+                for index, player in ipairs(players) do
+                    local playerPed = GetPlayerPed(player)
+
+                    -- check if ped exists to refrain from iterating potentially invalid player entities
+                    -- causes some players to not have blips if not double-checked
+                    if(DoesEntityExist(playerPed)) then
+                        local coords = GetEntityCoords(playerPed)
+
+                        -- build the blip object
+                        local obj = {
+                            player, NetworkGetNetworkIdFromEntity(playerPed),
+                            { coords.x, coords.y, coords.z }
+                        }
+
+                        -- if sendNames is enabled, then move coords up an index and insert the
+                        -- players name as the third, since that is what the client expects
+                        if(shouldSendNames) then
+                            obj[4] = obj[3]
+                            obj[3] = GetPlayerName(player)
+                        end
+
+                        table.insert(blips, obj)
+                    end
+                end
+
+                -- create another thread to quickly move-on to the next tick
+                Citizen.CreateThread(function()
+                    for index, player in ipairs(players) do
+                        if(DoesEntityExist(GetPlayerPed(player))) then
+                            local final = {}
+
+                            -- filter-out the players' blip from the blips array being sent
+                            for index, blip in ipairs(blips) do
+                                if(blip[1] ~= player) then
+                                    table.insert(final, blip)
+                                end
+                            end
+
+                            TriggerClientEvent("_bigmode:updateBlips", player, final)
+
+                            Citizen.Wait(math.clamp(10, updateSpacing, 100))
+                        end
+                    end
+                end)
+
+                lastBlipsUpdate = blips
+
+                -- if threadTimeWarnings is enabled, then calculate the time it took to run this thread
+                -- and if its above the threshold then send a warning to the server console
+                if(threadTimeWarnings) then
+                    local up_loopTime = GetGameTimer() - up_begin
+                    if(up_loopTime > updateThreadTimeThreshold) then
+                        print(string.format("[^2BigMode^7] Update thread loopTime: ^3%i ms ^7(your server is ^1lagging ^7or ^3updateThreadTimeThreshold ^7is too low)", up_loopTime))
+                    end
+                end
+            end)
+        end
+
+        -- if threadTimeWarnings is enabled, then calculate the time it took to run this thread
+        -- and if its above the threshold then send a warning to the server console
+        if(threadTimeWarnings) then
+            local mt_loopTime = GetGameTimer() - mt_begin
+            if(mt_loopTime > mainThreadTimeThreshold) then
+                print(string.format("[^2BigMode^7] Main thread loopTime: ^1%i ms ^7(your server is ^1lagging ^7or ^1mainThreadTimeThreshold ^7is too low)", mt_loopTime))
+            end
+        end
+
+        Citizen.Wait(updateInterval)
+    end
+end)
